374d3737a18058bbae3c625f1962dd13a6fb2de2
Dima239811 <dimacekalin2311@yandex.ru>
2025-07-02 15:12:58 +0300

    выполнил третье задание

---
+++ src/Task-3/3/Product/IProduct.java
@@ -0,0 +1,9 @@
+package Product;
+
+import ProductParts.IProductPart;
+
+public interface IProduct {
+    void installFirstPart(IProductPart iProductPart);
+    void installSecondPart(IProductPart iProductPart);
+    void installThirdPart(IProductPart iProductPart);
+}
